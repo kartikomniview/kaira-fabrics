261927d46a656c9b17d422f695f7d041b035a6fc
kartikomniview <71684610+kartikomniview@users.noreply.github.com>
2026-05-16 20:36:47 +0530

    UI changes

--- src/pages/aivisualizer/MaterialsInventory.tsx
+++ src/pages/aivisualizer/MaterialsInventory.tsx
@@ -101,7 +101,7 @@ export const MaterialsInventory = ({ onBack, onSelectMaterial, selectedMaterialI
     // Reset activeCollection when material type changes
     useEffect(() => {
         if (activeMaterialType === 'All') {
-            setActiveCollection('Koral ')
+            setActiveCollection('Koral')
         } else {
             const firstInType = collectionsWithThumbs[0]
             setActiveCollection(firstInType ? firstInType.name : 'All')
