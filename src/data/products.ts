b0a8d8f37ffb57bf1909ab7ce7fa307bb0679fcd
kartikomniview <71684610+kartikomniview@users.noreply.github.com>
2026-08-04 14:26:10 +0530

    model issues

--- src/data/products.ts
+++ src/data/products.ts
@@ -49,7 +49,7 @@ export const kairaProducts: KairaProduct[] = [
     id: 7,
     product_name: 'Marcellus',
     model_url: 'https://kairafabrics.s3.ap-south-1.amazonaws.com/ThreeAssets/productImages/v1/Marcellus.webp',
-    image_url: 'https://kairafabrics.s3.ap-south-1.amazonaws.com/ThreeAssets/models/v3/Marcellus.glb',
+    image_url: 'https://kairafabrics.s3.ap-south-1.amazonaws.com/ThreeAssets/models/v4/Marcellus.glb',
     ios_model_url: 'https://kairafabrics.s3.ap-south-1.amazonaws.com/ThreeAssets/models/ios/v3/Marcellus.glb',
     uvRotation: -90,
   },
